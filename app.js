06daafda7dbade84305b8806c08350d94f5f633a
marinalimacarvalho <marinaamil2007@gmail.com>
2026-04-12 23:14:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function () {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    if (document.body.classList.contains("light-theme")) {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+});
+
+console.log('current class name: ' + className);
